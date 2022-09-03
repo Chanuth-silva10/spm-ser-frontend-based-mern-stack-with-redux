fe7b9703d0d4c3528c0dbe4bfafc39b998e10aac
Chanuth10 <it20074418@my.sliit.lk>
2022-09-03 22:25:44 +0530

    Adding header and footer

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const Header = () => {
+  return (
+    <div>
+        Header
+    </div>
+);
+};
+export default Header;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
     return (
         <div>
-            Home
+          <Header/>
+          <Footer/>
         </div>
     );
 };
