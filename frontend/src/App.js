ed60d80c7bd71714a3c6988c4dd29bd857138166
Chanuth10 <it20074418@my.sliit.lk>
2022-09-05 20:43:28 +0530

    Add user reduers, actions and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Home from "./component/Home/Home";
+import UserData from './more/UserData';
+import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LoginSignup from "./component/Authentication/LoginSignup";
 
 function App() {
+
+  const {isAuthenticated,user} = useSelector((state) =>state.user);
+  
   return (
     <Router>
+       {isAuthenticated && <UserData user={user} />}
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={LoginSignup} />
       </Switch>
     </Router>
   );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,8 +3,15 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productsReducer } from "./reducers/ProductReducer";
 
+import { allUsersReducer, forgotPasswordReducer, profileReducer, userDetailsReducer, userReducer } from "./reducers/userReducer";
+
 const reducer = combineReducers({
   products: productsReducer,
+  allUsers: allUsersReducer,
+  forgotPassword:forgotPasswordReducer,
+  userDetails: userDetailsReducer,
+  profile: profileReducer,
+  user: userReducer,
 });
 
 const middleWare = [thunk];
