6d279b3e072d480b92bf4269d4a43c2f68b0e737
Chanuth10 <it20074418@my.sliit.lk>
2022-09-05 20:48:11 +0530

    Load user when browswer refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
 import Home from "./component/Home/Home";
-import UserData from './more/UserData';
-import { useSelector } from 'react-redux';
+import UserData from "./more/UserData";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginSignup from "./component/Authentication/LoginSignup";
+import WebFont from "webfontloader";
+import Store from "./store";
+import { loadUser } from "./actions/userAction";
 
 function App() {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
 
-  const {isAuthenticated,user} = useSelector((state) =>state.user);
-  
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+    
+    Store.dispatch(loadUser());
+  }, []);
   return (
     <Router>
-       {isAuthenticated && <UserData user={user} />}
+      {isAuthenticated && <UserData user={user} />}
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={LoginSignup} />
