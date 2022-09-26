9c1dfdbd11680246268a1db491a54d29b82b9ad3
ImashaKuruppu25 <binarydemonss@gmail.com>
2022-09-26 23:48:07 +0530

    Create Order reducers, actions, constants and update store.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,6 +16,14 @@ import {
   userDetailsReducer,
   userReducer,
 } from "./reducers/userReducer";
+import {
+  allOrdersReducer,
+  myOrdersReducer,
+  newOrderReducer,
+  orderDetailsReducer,
+  orderReducer,
+} from "./reducers/OrderReducer";
+
 const reducer = combineReducers({
   products: productsReducer,
   productDetails: productDetailsReducer,
@@ -28,12 +36,36 @@ const reducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   favourite: favouriteReducer,
+  AllOrders: allOrdersReducer,
+  favourite: favouriteReducer,
+  order: newOrderReducer,
+  myOrder: myOrdersReducer,
+  myOrderDetails: orderDetailsReducer,
+  deleteOrder: orderReducer,
 });
+
+let initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+
+    shippingInfo: localStorage.getItem("shippingInfo")
+      ? JSON.parse(localStorage.getItem("shippingInfo"))
+      : {},
+  },
+  favourite: {
+    favouriteItems: localStorage.getItem("favouriteItems")
+      ? JSON.parse(localStorage.getItem("favouriteItems"))
+      : [],
+  },
+};
 
 const middleWare = [thunk];
 
 const store = createStore(
   reducer,
+  initialState,
   composeWithDevTools(applyMiddleware(...middleWare))
 );
 
