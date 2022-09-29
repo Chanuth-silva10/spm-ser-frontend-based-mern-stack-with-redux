a13f2a550a77913fa3ccd43b4ce13b59652e0ef0
ImashaKuruppu25 <binarydemonss@gmail.com>
2022-09-29 23:04:06 +0530

    Created Frontend pages from order and payment with redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import Home from "./component/Home/Home";
 import UserData from "./more/UserData";
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginSignup from "./component/Authentication/LoginSignup";
@@ -17,9 +18,23 @@ import CreateProduct from "./component/Admin/CreateProduct";
 import EditProduct from "../../frontend/src/component/Admin/EditProduct";
 import AllUsers from "../../frontend/src/component/Admin/AllUsers";
 import UpdateUser from "../../frontend/src/component/Admin/UpdateUser";
+import Shipping from "./component/Cart/Shipping";
+import ConfirmOrder from "./component/Cart/ConfirmOrder";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Payment from "./component/Cart/Payment";
+import Notfound from "../../frontend/src/more/Notfound";
 
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
+
+  const [stripeApiKey, setStripeApiKey] = useState("");
+
+  async function getStripeApiKey() {
+    const { data } = await axios.get("/api/v2/stripeapikey");
+    console.log(data);
+    setStripeApiKey(data.stripeApiKey);
+  }
 
   useEffect(() => {
     WebFont.load({
@@ -29,15 +44,22 @@ function App() {
     });
 
     Store.dispatch(loadUser());
+    getStripeApiKey();
   }, []);
   return (
     <Router>
       {isAuthenticated && <UserData user={user} />}
+      {stripeApiKey && (
+        <Elements stripe={loadStripe(stripeApiKey)}>
+          <ProtectedRoute exact path="/process/payment" component={Payment} />
+        </Elements>
+      )}
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/product/:id" component={ProductDetails} />
         <Route exact path="/login" component={LoginSignup} />
         <Route exact path="/cart" component={Cart} />
+        <ProtectedRoute exact path="/shipping" component={Shipping} />
         <ProtectedRoute
           isAdmin={true}
           exact
@@ -73,6 +95,13 @@ function App() {
           exact
           path="/admin/user/:id"
           component={UpdateUser}
+        />
+        <ProtectedRoute exact path="/order/confirm" component={ConfirmOrder} />
+
+        <Route
+          component={
+            window.location.pathname === "/process/payment" ? null : Notfound
+          }
         />
       </Switch>
     </Router>
